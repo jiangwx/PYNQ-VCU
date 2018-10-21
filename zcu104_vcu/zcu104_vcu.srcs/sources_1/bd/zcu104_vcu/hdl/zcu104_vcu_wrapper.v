950d6c356d9f168a177c35795e974b08f79de88c
jiangwx <christophebigbear@163.com>
2018-10-21 14:05:54 +0800

    init PYNQ-VCU

---
+++ zcu104_vcu/zcu104_vcu.srcs/sources_1/bd/zcu104_vcu/hdl/zcu104_vcu_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
+//Date        : Sun Oct 21 12:59:34 2018
+//Host        : XPS15 running 64-bit major release  (build 9200)
+//Command     : generate_target zcu104_vcu_wrapper.bd
+//Design      : zcu104_vcu_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module zcu104_vcu_wrapper
+   ();
+
+
+  zcu104_vcu zcu104_vcu_i
+       ();
+endmodule
